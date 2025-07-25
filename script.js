9d2b9101901e5b22ba120d2a3fd233f6dcb29a51
Dulce <dulce@Dulces-MacBook-Air.local>
2025-07-25 11:50:48 -0700

    Add B40S information and  timeline

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript file for Bitcoin Development Program Timeline
+// Currently no interactive features, but ready for future enhancements!
+
+console.log("B4OS Timeline loaded successfully!");
